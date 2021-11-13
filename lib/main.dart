6720e7a7353eaf82fda9e2bb4921e7dfb8764e14
Izaz Dhiyaulhaq <59567289+izazdhiya@users.noreply.github.com>
2021-11-13 18:16:40 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,5 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'APU PSP',
-      home: SplashScreenPage()));
+      debugShowCheckedModeBanner: false, title: 'APU PSP', home: Loading()));
 }
